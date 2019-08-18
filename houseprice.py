e60931e112fffc9a97754b3763621ef0dc9b28d6
botianzhe <ypp865@163.com>
2019-08-18 08:58:43 +0800

    erdfh

--- houseprice.py
+++ houseprice.py
@@ -157,15 +157,36 @@ encoder.classes_
 housing_cat_encoded, housing_categories = house_cat.factorize()
 
 #%%
+# OneHotEncoder
 from sklearn.preprocessing import OneHotEncoder
 encoder=OneHotEncoder()
 house_onehot=encoder.fit_transform(housing_cat_encoded.reshape(-1,1))
 house_onehot.toarray()
 
 #%%
+# 二者合并
 from sklearn.preprocessing  import CategoricalEncoder
 encoder=CategoricalEncoder()
 house_onehot=encoder.fit_transform(housing_cat.reshape(-1,1))
 house_onehot
 
 #%%
+# 自定义转换器
+from sklearn.base import BaseEstimator, TransformerMixin
+rooms_ix, bedrooms_ix, population_ix, household_ix = 3, 4, 5, 6
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+def __init__(self, add_bedrooms_per_room = True): # no *args or **kargs
+self.add_bedrooms_per_room = add_bedrooms_per_room
+def fit(self, X, y=None):
+return self # nothing else to do
+def transform(self, X, y=None):
+rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
+population_per_household = X[:, population_ix] / X[:, household_ix]
+if self.add_bedrooms_per_room:
+bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+return np.c_[X, rooms_per_household, population_per_household,
+bedrooms_per_room]
+else:
+return np.c_[X, rooms_per_household, population_per_household]
+attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+housing_extra_attribs = attr_adder.transform(housing.values)
